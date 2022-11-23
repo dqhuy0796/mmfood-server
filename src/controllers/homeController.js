ca847d1397cc611c3cb5a99707426f31c2239c88
dqhuy0796 <68587628+dqhuy0796@users.noreply.github.com>
2022-11-23 22:43:37 +0700

    first commit

---
+++ src/controllers/homeController.js
@@ -0,0 +1,7 @@
+let getHomepage = (req, res) => {
+    return res.render('home.ejs');
+}
+
+module.exports = {
+    getHomepage
+}
---
+++ src/server.js
@@ -0,0 +1,19 @@
+import bodyParser from "body-parser";
+import express from "express";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
+require('dotenv').config();
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initWebRoutes(app);
+
+let port = process.env.PORT || 9999;
+
+app.listen(port, () => {
+    console.log(port);
+});
